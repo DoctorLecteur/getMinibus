6bc2d325c21e3895dbd0193b782a075b0fa046a9
Doctor_Lector <roller97@yandex.ru>
2022-01-10 22:07:34 +0300

    Test Flask Templates HTML

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Привет, Мир!"
+    user = {'username': 'test123'}
+    posts = [
+        {
+            'author': {'username': 'Вован'},
+            'body': 'Test1'
+        },
+        {
+            'author': {'username': 'Русик'},
+            'body': 'Test2'
+        },
+        {
+            'author': {'username': 'Димас'},
+            'body': 'Test3'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
