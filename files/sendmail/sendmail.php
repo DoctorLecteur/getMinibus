493a66418e644d91d71ecc6200870df1fc9e8a09
CharismaRM <81681581+CharismaRM@users.noreply.github.com>
2022-01-16 14:48:14 +0300

    add first version site

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,54 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('', 'Фрилансер по жизни');
+	//Кому отправить
+	$mail->addAddress('');
+	//Тема письма
+	$mail->Subject = 'Привет! Это "Фрилансер по жизни"';
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+
+	//if(trim(!empty($_POST['name']))){
+		//$body.='';
+	//}	
+	
+	/*
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
